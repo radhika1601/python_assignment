6854682e95168517dd61327af4cb19b8d4a1bc26
Radhika <radhika@cs.iitr.ac.in>
2019-04-02 23:53:19 +0530

    config file added to gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(
-	host="localhost",
-	user="radhika",
-	passwd="Rg@756831")
-
-cursor = cnx.cursor()
-cursor.execute("use python_assignment")
